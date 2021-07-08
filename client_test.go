cd85f58e3cf70dc2bd07869e3c3a9df71862d721
mrz1836 <mrz1818@gmail.com>
2021-07-08 09:39:24 -0400

    Revert version back to 0.0.1

--- client_test.go
+++ client_test.go
@@ -283,7 +283,7 @@ func ExampleNewClient() {
 		return
 	}
 	fmt.Printf("loaded client: %s", client.options.userAgent)
-	// Output:loaded client: dotwallet-go-sdk: v0.0.6
+	// Output:loaded client: dotwallet-go-sdk: v0.0.1
 }
 
 // BenchmarkNewClient benchmarks the method NewClient()
